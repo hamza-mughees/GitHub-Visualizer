d5975d639099e091050d6fe00330212615ce3b23
hamza-mughees <mugheesh@tcd.ie>
2020-11-30 21:08:24 +0000

    Successfully accessing api

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+var data;
 
+async function accessApi() {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const token = document.getElementById("token").value
+    //alert(token);
+    data = await getData(username, token).catch(e => console.error(e));
+    //console.log(result);
+    console.log(data)
+}
 
-function pressButton() {
-    console.log('Clicked')
+async function getData(username, token) {
+    let url = `https://api.github.com/users/${username}/repos`;
+
+    const headers = {
+        'Authorization': `Token ${token}`
+    }
+
+    const response = await fetch(url, {
+        'method': 'GET',
+        'headers': headers
+    });
+
+    let data = await response.json();
+    
+    return data;
 }
