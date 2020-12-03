ab8927d70c14d2adc66fc0bfac885704b9fffc30
hamza-mughees <mugheesh@tcd.ie>
2020-12-03 01:28:07 +0000

    Added language visualization

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
-let username;
-let token;
-
-let userInfo;
-let userRepos;
-
-let repoNames;
-let repoSizes;
-
 async function accessApi() {
     event.preventDefault();
     
-    username = document.getElementById("username").value
-    token = document.getElementById("token").value
+    let username = document.getElementById("username").value
+    let token = document.getElementById("token").value
     
     displayLoader("info-container")
-    displayLoader("visualizations")
+    displayLoader("visualization-1")
 
-    userInfo = await getUserInfo()
-    displayUserInfo()
+    let userInfo = await getUserInfo(username, token)
+    displayUserInfo(userInfo)
 
-    userRepos = await getUserRepos()
+    let userRepos = await getUserRepos(username, token)
 
-    repoSizes = getUserRepoSizes()
-    repoNames = getUserRepoNames()
-    displayRepoSizesGraph()
+    let repoLanguages = exploitData(userRepos, 'language')
+    displayRepoLanguagesGraph(repoLanguages)
+
+    let repoSizes = exploitData(userRepos, 'size')
+    let repoNames = exploitData(userRepos, 'name')
+    displayRepoSizesGraph(repoNames, repoSizes)
 }
 
 async function getData(url, token) {
@@ -39,12 +33,12 @@ async function getData(url, token) {
     return await response.json()
 }
 
-async function getUserInfo() {
+async function getUserInfo(username, token) {
     let url = `https://api.github.com/users/${username}`
     return await getData(url, token).catch(e => console.error(e))
 }
 
-async function getUserRepos() {
+async function getUserRepos(username, token) {
     let url = `https://api.github.com/users/${username}/repos`
     return await getData(url, token).catch(e => console.error(e))
 }
@@ -53,7 +47,7 @@ function displayLoader(divId) {
     document.getElementById(divId).innerHTML = `<div class="loader"></div>`
 }
 
-function displayUserInfo() {
+function displayUserInfo(userInfo) {
     document.getElementById("info-container").innerHTML = `
         <div class="card" onclick="window.open('${userInfo.html_url}', '_blank')">
             <img id="avatar" src="${userInfo.avatar_url} alt="Avatar"/>
@@ -70,24 +64,74 @@ function displayUserInfo() {
     `
 }
 
-function getUserRepoSizes() {
-    return userRepos.map(function(repo) {
-        return repo.size
+function exploitData(data, attribute) {
+    return data.map(function(sample) {
+        return sample[attribute]
     })
 }
 
-function getUserRepoNames() {
-    return userRepos.map(function(repo) {
-        return repo.name
-    })
+function getLanguagesMap(repoLanguages) {
+    var map = new Map()
+    for (language of repoLanguages) {
+        map.set(language, map.has(language) ? map.get(language) + 1 : 1)
+    }
+    return map
 }
 
-function displayRepoSizesGraph() {
-    document.getElementById("visualizations").innerHTML = `
-    <div id="repo-sizes-graph-container">
-        <canvas id="repo-sizes-chart" width="200" height="150"></canvas>
+function displayRepoLanguagesGraph(repoLanguages) {
+    let languagesMap = getLanguagesMap(repoLanguages)
+
+    let languageNames = Array.from(languagesMap.keys())
+    let languageQuantities = Array.from(languagesMap.values())
+
+    let colours = randomColours(languageNames)
+
+    document.getElementById("visualization-1").innerHTML = `
+    <div id="repo-languages-graph-container">
+        <canvas id="repo-languages-chart" width="400" height="200"></canvas>
     </div>
     `
+
+    var ctx = document.getElementById('repo-languages-chart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: languageNames,
+            datasets: [{
+                label: 'Repository Language',
+                data: languageQuantities,
+                backgroundColor: colours,
+                borderColor: colours,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+function randomColours(labels) {
+    return labels.map(function() {
+        return `rgba(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, 0.7)`
+    })
+}
+
+function displayRepoSizesGraph(repoNames, repoSizes) {
+    let colours = randomColours(repoNames)
+
+    document.getElementById("visualization-2").innerHTML = `
+    <div id="repo-sizes-graph-container">
+        <canvas id="repo-sizes-chart" width="400" height="200"></canvas>
+    </div>
+    `
+
     var ctx = document.getElementById('repo-sizes-chart').getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'bar',
@@ -96,8 +140,8 @@ function displayRepoSizesGraph() {
             datasets: [{
                 label: 'Repository Size',
                 data: repoSizes,
-                backgroundColor: 'rgba(253, 160, 133, 0.2)',
-                borderColor: 'rgba(253, 160, 133, 1)',
+                backgroundColor: colours,
+                borderColor: colours,
                 borderWidth: 1
             }]
         },
